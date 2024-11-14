34b34e1a2a171119c8476cc842bb23c97684b6e4
TommyTommy10 <tommaso.grade@gmail.com>
2024-11-14 21:57:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,23 @@ var map = L.map('map', {
 });
 
 var bounds = [[0,0], [1000,1000]];
-var image = L.imageOverlay('https://github.com/TommyTommy10/hollow-knight/raw/main/Hallownest%20map.png', bounds).addTo(map);
+var image = L.imageOverlay('path/to/hallownest_map.png', bounds).addTo(map);
 
 map.fitBounds(bounds);
 
-// Aggiungi marker per alcune aree di Hallownest
-var greenPath = L.marker([400, 200]).addTo(map)
-    .bindPopup('Greenpath')
+// Aggiungi marker per le aree di Hallownest
+var dirtmouth = L.marker([220, 760]).addTo(map)
+    .bindPopup('Dirtmouth')
     .openPopup();
 
-var cityOfTears = L.marker([600, 500]).addTo(map)
+var greenPath = L.marker([350, 600]).addTo(map)
+    .bindPopup('Greenpath');
+
+var cityOfTears = L.marker([500, 400]).addTo(map)
     .bindPopup('City of Tears');
 
-var crystalPeak = L.marker([300, 700]).addTo(map)
-    .bindPopup('Crystal Peak');
+var kingdomsEdge = L.marker([700, 200]).addTo(map)
+    .bindPopup("Kingdom's Edge");
+
+var theAbyss = L.marker([900, 100]).addTo(map)
+    .bindPopup('The Abyss');
